fb7980b7073587457e1768851fedba05ef259165
Sager Alarifi <sagr.1993@gmail.com>
2020-03-18 16:30:28 +0300

    Updates

---
+++ precache-manifest.bc5a2e1ef2a2fc629309a2e0d0e7d40d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acc483c00d1ab7a5572a25d786853ea",
+    "url": "/react-template/index.html"
+  },
+  {
+    "revision": "acd760eed5ded5ccdff7",
+    "url": "/react-template/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "69682b2f7220dc55d95d",
+    "url": "/react-template/static/css/main.22a540e9.chunk.css"
+  },
+  {
+    "revision": "acd760eed5ded5ccdff7",
+    "url": "/react-template/static/js/2.c05864e4.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/react-template/static/js/2.c05864e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69682b2f7220dc55d95d",
+    "url": "/react-template/static/js/main.dea78fa5.chunk.js"
+  },
+  {
+    "revision": "55b25fd3d1632fa3bbb5",
+    "url": "/react-template/static/js/runtime-main.e44e70da.js"
+  }
+]);
